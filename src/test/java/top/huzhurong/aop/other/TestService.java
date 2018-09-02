d19e034bdd433e78e875e02e56f8a25ff537165f
chenshun00 <13786285072@163.com>
2018-09-02 09:11:57 +0800

    :white_check_mark: 添加声明式事务测试

---
+++ src/test/java/top/huzhurong/aop/other/TestService.java
@@ -0,0 +1,25 @@
+package top.huzhurong.aop.other;
+
+import top.huzhurong.aop.annotation.Transactional;
+
+/**
+ * @author luobo.cs@raycloud.com
+ * @since 2018/9/2
+ */
+public class TestService {
+
+    private TestDao testDao = new TestDao();
+
+    /**
+     * 测试事务回滚
+     */
+    @Transactional
+    public void addTest(Test test) {
+        testDao.addTest(test);
+        Test testById = testDao.getTestById(22);
+        testById.setName("陈顺测试");
+        testDao.updatetestById(testById);
+        int i = 10 / 0;
+    }
+
+}
---
+++ src/test/java/top/huzhurong/aop/other/TransactionTest.java
@@ -0,0 +1,57 @@
+package top.huzhurong.aop.other;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.Before;
+import org.junit.Test;
+import top.huzhurong.aop.advisor.Advisor;
+import top.huzhurong.aop.advisor.transaction.TransactionAdvisor;
+import top.huzhurong.aop.advisor.transaction.manager.JdbcTransactionManager;
+import top.huzhurong.aop.core.AspectjParser;
+
+import java.util.List;
+
+/**
+ * @author luobo.cs@raycloud.com
+ * @since 2018/9/2
+ */
+public class TransactionTest {
+
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/test?useSSL=false&amp;useUnicode=true&amp;characterEncoding=utf-8&amp;autoReconnect=true";
+    private static final String user = "root";
+    private static final String password = "chenshun";
+
+    private TestService testService;
+    private DruidDataSource druidDataSource;
+
+    @Before
+    public void before() {
+        testService = new TestService();
+
+        druidDataSource = new DruidDataSource();
+        druidDataSource.setMaxActive(10);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(user);
+        druidDataSource.setPassword(password);
+        druidDataSource.setValidationQuery("select 'x'");
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setTestWhileIdle(true);
+    }
+
+    @Test
+    public void TestTransaction() throws Exception {
+
+        top.huzhurong.aop.other.Test testAdd = top.huzhurong.aop.other.Test.builder().id(23).age(222).name("test add again").build();
+
+        List<Advisor> advisors = AspectjParser.parserAspectj(TransactionTest.class);
+        for (Advisor advisor : advisors) {
+            if (advisor instanceof TransactionAdvisor) {
+                TransactionAdvisor transactionAdvisor = (TransactionAdvisor) advisor;
+                JdbcTransactionManager transactionManager = (JdbcTransactionManager) transactionAdvisor.getTransactionManager();
+                transactionManager.setDataSource(druidDataSource);
+            }
+        }
+        TestService testServiceProxy = (TestService) AspectjParser.findApplyAdvisor(testService, advisors);
+        testServiceProxy.addTest(testAdd);
+    }
+}
